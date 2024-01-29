d404d020f541bab9797ccef9ab1fd58aea094963
isetfst <ayoubbenmossbeh@gmail.com>
2024-01-29 13:42:51 +0100

    Comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,11 @@
+/*
+ * comment
+ */
+
 package ma.enset;
 public class Test{
     public static void main(String[] args){
+        //**********************************
         System.out.println("hello word");
     }
 }
