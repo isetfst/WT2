191bf1cfe80f29896b8774adb10b37616e915caf
isetfst <ayoubbenmossbeh@gmail.com>
2024-01-31 14:44:28 +0100

    Pach code

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -7,5 +7,6 @@ public class Test{
     public static void main(String[] args){
         //**********************************
         System.out.println("hello word");
+        System.out.println("hello word");
     }
 }
